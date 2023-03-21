f7f954f01cf36f6f3d95be0ea9328e7460949616
gabimtolentino <gabimonica.t@gmail.com>
2023-03-20 18:41:38 -0700

    made changes to signup and login page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -6,14 +6,13 @@ import Cart from "../components/Cart";
 const Home = () => {
   return (
     <div className="container">
-      <div className="card bg-white card-rounded">
-    <div className="card-header bg-dark text-center">
+    <div className="landing">
       <h1>Buy A Star, Save Hubble!</h1>
     </div>
-    <div className="card-body m-5">
+    <div className="landing2">
       <h2>How many people can say they own their very own star?</h2>
     </div>
-    <div className="card-footer text-center m-3">
+    <div className="landing3">
       <h2>The James Webb Telescope has bumped Hubble down to the second best telescope ever built. 
         If you're just as in love with Hubble as us, now is your chance to save Hubble! All purchases 
         will go toward advancing and improving the Hubble Telescope so that we can bring it back to 
@@ -21,7 +20,6 @@ const Home = () => {
       </h2>
     </div>
   </div>
-    </div>
   );
 };
 
