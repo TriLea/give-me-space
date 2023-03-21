6d2e4015be5b0da36d8a54c920d8d038fcd5b3e1
gabimtolentino <gabimonica.t@gmail.com>
2023-03-20 22:29:59 -0700

    changed title and added image

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -6,13 +6,22 @@ import Cart from "../components/Cart";
 const Home = () => {
   return (
     <div className="container">
-    <div className="landing">
+      <div style={{display:"flex",justifyContent:"center"}}>
+      <div style={{padding:"48px"}} id="land" className="landing">
       <h1>Buy A Star, Save Hubble!</h1>
     </div>
-    <div className="landing2">
+      </div>
+    <div id="land2" className="landing2">
       <h2>How many people can say they own their very own star?</h2>
     </div>
-    <div className="landing3">
+    <div>
+    <img src={process.env.PUBLIC_URL+'/HubblePic.jpeg'} style={{width:"400px",height:"300",justifyContent:"center"}}/>
+    </div>
+    <div>
+    </div>
+    <div style={{display:"flex",justifyContent:"center"}}>
+      </div>
+    <div style={{padding:"40px",color:"rgb(5, 18, 117)"}} className="landing3">
       <h2>The James Webb Telescope has bumped Hubble down to the second best telescope ever built. 
         If you're just as in love with Hubble as us, now is your chance to save Hubble! All purchases 
         will go toward advancing and improving the Hubble Telescope so that we can bring it back to 
